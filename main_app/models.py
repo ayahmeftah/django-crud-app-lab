3554e212581e9b8e19cdb5298fd34f9c157051ac
ayahmeftah <ayahmeftah123123@gmail.com>
2025-08-31 19:06:20 +0300

    created models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Owner(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    class Meta:
+        db_table = 'owner'
+
+    def __str__(self):
+        return self.name
+    
+class Electronic(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=50)
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name="electronics")
+
+    class Meta:
+        db_table = 'electronic'
+
+    def __str__(self):
+        return f"{self.name} ({self.type})"
